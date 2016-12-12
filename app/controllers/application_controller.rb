e677f2c6ba5cb11f23ba204370f86ab37a828c97
blake <blakegeist@gmail.com>
2016-12-11 16:46:40 -0800

    remove the last

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :add_www_subdomain
-
-  private
-  def add_www_subdomain
-   unless /^www/.match(request.host)
-     redirect_to("#{request.protocol}www.#{request.host_with_port}",status: 301)
-   end
-  end
-
 end
