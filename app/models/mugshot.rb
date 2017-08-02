02ac79641e9e5ab73e892ba1bc3e3061ad4f0b73
blake <blakegeist@gmail.com>
2017-08-01 18:32:07 -0700

    update slugged url used and horry county list
    update slugged url used and horry county list

--- app/models/mugshot.rb
+++ app/models/mugshot.rb
@@ -3,7 +3,7 @@ class Mugshot < ActiveRecord::Base
   has_many :photos, :dependent => :destroy
   has_many :charges, :dependent => :destroy
   extend FriendlyId
-  friendly_id :name, use: :history
+  friendly_id :slug_candidates, use: :slugged
 
   def previous
     county.mugshots.where(["id > ?", id]).first
@@ -12,4 +12,18 @@ class Mugshot < ActiveRecord::Base
   def next
     county.mugshots.where(["id < ?", id]).last
   end
+
+  friendly_id :slug_candidates, use: :slugged
+
+
+ def slug_candidates
+   [:name, :name_and_sequence]
+ end
+
+ def name_and_sequence
+   slug = name.to_param
+   sequence = Mugshot.where(name: "#{slug}-").count + 2
+   "#{slug}--#{sequence}"
+ end
+
 end
