b0f30cc572d2676c993ab94534f98eca4f3d3bda
blake <blakegeist@gmail.com>
2017-08-01 20:32:35 -0700

    update for multi offender list
    update for multi offender list

--- app/controllers/mugshots_admin_controller.rb
+++ app/controllers/mugshots_admin_controller.rb
@@ -4,6 +4,10 @@ class MugshotsAdminController < ApplicationController
       redirect_to root_path
     end
     @mugshots = Mugshot.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 100)
+
+    #multi offender list
+    @mugshots = Mugshot.where(name: Mugshot.select(:name).group(:name).having('count(*) > 1')).order("created_at DESC").paginate(:page => params[:page], :per_page => 100)
+
     respond_to do |format|
       format.js
       format.html
